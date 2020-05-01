e431d5a4e0e6c2362ef5cb4001cbcdd8e4c3872d
ABTCoder <amalbenson98@gmail.com>
2020-05-01 11:58:45 +0200

    File update test 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Rick</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
