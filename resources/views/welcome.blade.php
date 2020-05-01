ca5672f677ac30e99d4efbe09ac1201f5b534b3c
ABTCoder <amalbenson98@gmail.com>
2020-05-01 11:36:48 +0200

    File update test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    OASIS
                 </div>
 
                 <div class="links">
