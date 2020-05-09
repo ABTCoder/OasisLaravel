04fca8e90edaa4ae26bf8528edb3b239907cafdc
ABTCoder <amalbenson98@gmail.com>
2020-05-09 12:12:33 +0200

    Unico javascript

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Prodotti')
 
 @section('asset')
-<script type="text/javascript" src="{{ asset('js/categories.js') }}"></script>
 <link rel="stylesheet" type="text/css" href="{{ asset('css/products.css') }}" >
 @endsection
 
