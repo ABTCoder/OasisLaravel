56e7ac698ec6c727bc396f04c2d67525e14cdbef
Federico <federico.panackal@outlook.com>
2020-05-11 12:45:36 +0200

    Fix Pagina 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.public')
-@section('title', 'Pagina non trovata')
-@section('content')
 
+@section('title', 'Pagina non trovata')
+
+@section('content')
 <img id="not_found" src="{{ asset('images/ui_images/oasis_home_background.jpg') }}">
 <div class="centered">
-    <div id="boh">
+    
     <div id="big">
         404 |
     </div>
     <div id="small">
     La pagina che stai cercando non esiste 
     <br>
-    Torna alla <a href="{{route('home')}}"> Home</a> 
+    <span>Torna alla <a href="{{route('home')}}"> Home</a> </span>
     </div>
-</div>
+
 </div>
 @endsection
