e39173d79a5236233d8e7dd2a2684a29e35610d9
GiovanniGregorini <giovigre@gmail.com>
2020-05-13 12:46:51 +0200

    aggiornamento javascript

--- public/js/main.js
+++ public/js/main.js
@@ -31,12 +31,13 @@ window.onload = function() {
 		
 	//CATEGORIE PRODOTTI	
 	var cat = document.getElementsByClassName("cat");
-        
+       
+        var i;
 	// Show or hide the subcat when a cat is clicked
-    for(var i = 0; i < cat.length; i++) {
-        cat[i].children[0].addEventListener("click",function() {
-            this.classList.toggle("rotate");
-            this.nextElementSibling.classList.toggle("show");
-        });
-    }
+        for(i = 0; i < cat.length; i++) {
+            cat[i].children[1].addEventListener("click",function() {
+                this.parentElement.classList.toggle("rotate");
+                this.parentElement.children[2].classList.toggle("show");
+            });
+        }
 };
