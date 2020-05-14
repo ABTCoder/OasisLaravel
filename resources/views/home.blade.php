c685653ed670a7abf11a19773fa67794dc0a8ff9
matteotoma98 <matteotoma98@hotmail.it>
2020-05-14 12:03:22 +0200

    Modifica allo scopo del sito

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,13 @@
 
 @section('content')
 <div class="home_box">
-<img id="coverpic" src="{{ asset('images/ui_images/oasis_home.jpg') }}">
-<div class="mission_content">
-			<h1 class="mission_title">Il miglior catalogo di articoli di elettronica</h1>
-			<h4 class="mission">Il nostro catalogo offre un catalogo, con una vasta gamma di prodotti elettronici. Lo scopo di questo sito è quello di offrire agli utenti di visionare i prodotti che noi vendiamo. (Da Modificare)</h4>
-			</div>
+    <img id="coverpic" src="{{ asset('images/ui_images/oasis_home.jpg') }}">
+    <div class="mission_content">
+        <h1 class="mission_title">Il miglior catalogo di articoli di elettronica</h1>
+        <h4 class="mission">Questo sito offre un catalogo, con una vasta gamma di prodotti elettronici. <br>
+            Lo scopo di questo sito è quello di offrire agli utenti una visualizzazione rapida e ben organizzata <br>
+            dei prodotti in magazzino, 
+            con la possibilità di cercare un determinato prodotto che si desidera acquistare.</h4>
+    </div>
 </div>
 @endsection
