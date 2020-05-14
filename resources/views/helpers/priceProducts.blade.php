6769e087f354caf2c39c523a85b61a653102c039
ABTCoder <amalbenson98@gmail.com>
2020-05-14 11:38:00 +0200

    Helper fix

--- resources/views/helpers/priceProducts.blade.php
+++ resources/views/helpers/priceProducts.blade.php
@@ -1,9 +1,9 @@
 @if ( $product->sconto == 0 ) 
-<div class="og_price"> {{ number_format($product->getPrice(), 2, ',', '.') }} € </div>
+<div class="og_price"> {{ number_format($product->getPrice(), 2, ',', '.') }}€ </div>
 @else
-<div class="og_price_crossed">{{ number_format($product->getPrice(), 2, ',', '.') }} €</div>
-<div class="discount">-{{ %product->sconto }}% Sconto</div>
-<div class="discounted_price">{{ number_format($product->getPrice(true), 2, ',', '.') }} €</div>
+<div class="og_price_crossed">{{ number_format($product->getPrice(), 2, ',', '.') }}€</div>
+<div class="discount">-{{ $product->sconto }}% Sconto</div>
+<div class="discounted_price">{{ number_format($product->getPrice(true), 2, ',', '.') }}€</div>
 @endif
 
 
