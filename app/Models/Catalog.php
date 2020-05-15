bdc7f17a1486633d8492b0ade3893cfe5dc90672
ABTCoder <amalbenson98@gmail.com>
2020-05-15 11:31:47 +0200

    Fix seleziona prodotto

--- app/Models/Catalog.php
+++ app/Models/Catalog.php
@@ -28,7 +28,7 @@ class Catalog {
 	}
 	
 	// Estrae i prodotti della categoria (tutti o solo quelli in sconto), eventualmente ordinati
-    public function getProdsBySubCat($catName, $paged = 9, $order = null, $discounted = true) {
+    public function getProdsByCat($catName, $paged = 9, $order = null, $discounted = true) {
 
         $prods = Product::whereHas('prodCat', function ($query) use ($catName) {
                         $query->whereIn('categoria', $catName);
