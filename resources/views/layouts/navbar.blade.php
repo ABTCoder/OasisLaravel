2fde5b05ae9e8410a45b2b749dd9d79efd0b0364
Federico <federico.panackal@outlook.com>
2020-05-19 11:06:36 +0200

    NavBar con icone

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,17 +1,20 @@
 <nav id="topnav">
     <a class="homelink" href="{{ route('home') }}">
-        <img src="{{ asset('images/ui_images/logo.png') }}">
+        <img class="sitelogo" src="{{ asset('images/ui_images/logo.png') }}">
         <span> OASIS </span>
     </a>
     <ul>
-        <li class="{{ Route::is('home*') ? 'active' : '' }}"><a href="{{ route('home') }}">HOME</a></li>
-        <li class="{{ Route::is('products*') ? 'active' : '' }}"><a href="{{ route('products') }}">PRODOTTI</a></li>
-        <li class="{{ Route::is('howtobuy*') ? 'active' : '' }}"><a href="{{ route('howtobuy') }}">COME ACQUISTARE</a></li>
+        <li class="{{ Route::is('home*') ? 'active' : '' }}"><a href="{{ route('home') }}"> 
+			<img class="nav_item_icon"src="{{ asset('images/ui_images//navicons/nav_home.png') }}">HOME</a></li>
+        <li class="{{ Route::is('products*') ? 'active' : '' }}"><a href="{{ route('products') }}">
+			<img class="nav_item_icon"src="{{ asset('images/ui_images//navicons/nav_products.png') }}">PRODOTTI</a></li>
+        <li class="{{ Route::is('howtobuy*') ? 'active' : '' }}"><a href="{{ route('howtobuy') }}">
+			<img class="nav_item_icon"src="{{ asset('images/ui_images//navicons/nav_buyinfo.png') }}">COME ACQUISTARE</a></li>
         <li class="{{ Route::is(['login*', 'signup*']) ? 'active' : '' }}" id="account">
             <div class="dropdown">
                 <a id="profile">
-                    ACCOUNT 
-                    <img src="{{ asset('images/ui_images/profile.png') }}">
+					<img src="{{ asset('images/ui_images/profile.png') }}">
+                    ACCOUNT
                 </a>
                 <div class="dropdown-content">
                     <a href="{{ route('login') }}">ACCEDI</a>
@@ -19,7 +22,9 @@
                 </div>
             </div>
         </li>
-        <li class="{{ Request::is('staff*') ? 'active' : '' }}" id="staff"><a href="{{ route('staff') }}"> AREA STAFF</a></li>
-        <li class="{{ Route::is('admindashboard*') ? 'active' : '' }}" id="admin"><a href="{{ route('admindashboard') }}"> AREA ADMIN</a></li>
+        <li class="{{ Route::is('staff*') ? 'active' : '' }}" id="staff"><a href="{{ route('staff') }}"> 
+			<img class="nav_item_icon"src="{{ asset('images/ui_images//navicons/nav_staff.png') }}">AREA STAFF</a></li>
+        <li class="{{ Route::is('admindashboard*') ? 'active' : '' }}" id="admin"><a href="{{ route('admindashboard') }}"> 
+			<img class="nav_item_icon"src="{{ asset('images/ui_images//navicons/nav_admin.png') }}">AREA ADMIN</a></li>
     </ul>
 </nav>
