6d529f0b535312fe44a75824e2fc52c7687edad8
ABTCoder <amalbenson98@gmail.com>
2020-05-22 20:33:56 +0200

    Importazione Jquery nel layout public.blade

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -6,6 +6,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+		<script type="text/javascript" src="{{ asset('js/jquery-3.5.1.min.js') }}" ></script>
 		<script type="text/javascript" src="{{ asset('js/main.js') }}" ></script>
 		@yield('asset')
     </head>
