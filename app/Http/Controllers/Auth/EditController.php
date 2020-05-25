4b07e5abd7f8d191ea5c8163e83cfb0b4371d6f6
ABTCoder <amalbenson98@gmail.com>
2020-05-25 12:24:35 +0200

    Modifica account test

---
+++ app/Http/Controllers/Auth/EditController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller {
+    /*
+      |--------------------------------------------------------------------------
+      | Register Controller
+      |--------------------------------------------------------------------------
+      |
+      | This controller handles the registration of new users as well as their
+      | validation and creation. By default this controller uses a trait to
+      | provide this functionality without requiring any additional code.
+      |
+     */
+
+use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    //protected $redirectTo = '/home';
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data) {
+        return Validator::make($data, [
+                    'nome' => ['required', 'string', 'max:20'],
+                    'cognome' => ['required', 'string', 'max:20'],
+                    'email' => ['required', 'string', 'email', 'max:50', 'unique:utente'],
+                    'username' => ['required', 'string', 'min:5', 'unique:utente'],
+                    'password' => ['required', 'string', 'min:8', 'confirmed'],
+					'residenza' => ['required', 'string', 'max:50'],
+					'data_nasc' => ['required'],
+					'occupazione' => ['required'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data) {
+        return User::create([
+                    'nome' => $data['nome'],
+                    'cognome' => $data['cognome'],
+                    'email' => $data['email'],
+                    'username' => $data['username'],
+                    'password' => Hash::make($data['password']),
+                    'occupazione' => $data['occupazione'],
+                    'residenza' => $data['residenza'],
+                    'data_nasc' => $data['data_nasc']
+        ]);
+    }
+
+}
--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -10,46 +10,16 @@
 <div class="account_box">
     <h2> Modifica </h2>
 	{{ Form::open(array('route' => 'editaccount.save')) }}
-		{{ Form::text('nome', '', ['placeholder' => 'Nome', 'id' => 'nome']) }}
+		{{ Form::text('nome', null, ['placeholder' => 'Nome', 'id' => 'nome']) }}
 		@if ($errors->first('nome'))
 			@foreach ($errors->get('nome') as $message)
 			<div id="error_msg">{{ $message }}</div>
 			@endforeach
 		@endif
         <br>
-		{{ Form::text('cognome', '', ['placeholder' => 'Cognome', 'id' => 'cognome']) }}
+		{{ Form::text('cognome', null, ['placeholder' => 'Cognome', 'id' => 'cognome']) }}
 		@if ($errors->first('cognome'))
 			@foreach ($errors->get('cognome') as $message)
-			<div id="error_msg">{{ $message }}</div>
-			@endforeach
-		@endif
-        <br>
-		{{ Form::text('username', '', ['placeholder' => 'Username', 'id' => 'username']) }}
-		@if ($errors->first('username'))
-			@foreach ($errors->get('username') as $message)
-			<div id="error_msg">{{ $message }}</div>
-			@endforeach
-		@endif
-        <br>
-		{{ Form::text('email', '', ['placeholder' => 'Email', 'id' => 'email']) }}
-		@if ($errors->first('email'))
-			@foreach ($errors->get('email') as $message)
-			<div id="error_msg">{{ $message }}</div>
-			@endforeach
-		@endif
-        <br>
-		{{ Form::password('password', ['placeholder' => 'Password', 'id' => 'password']) }}
-		@if ($errors->first('password'))
-			@foreach ($errors->get('password') as $message)
-			<div id="error_msg">{{ $message }}</div>
-			@endforeach
-		@endif
-		<br>
-		{{ Form::password('password_confirmation', ['placeholder' => 'Conferma Password', 'id' => 'password-confirm']) }}
-        <br>
-		{{ Form::text('residenza', null, ['placeholder' => 'Indirizzo di residenza', 'id' => 'residenza']) }}
-		@if ($errors->first('residenza'))
-			@foreach ($errors->get('residenza') as $message)
 			<div id="error_msg">{{ $message }}</div>
 			@endforeach
 		@endif
@@ -72,9 +42,8 @@
 			@endforeach
 		@endif
         <br>
-		{{ Form::submit('Registrati') }}
+		{{ Form::submit('Modifica') }}
         <br>
 	{{ Form::close() }}
-    <b> Possiedi già un account? <a href="{{ route('login') }}"> ACCEDI </a> </b>
 </div>
 @endsection
