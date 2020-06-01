3f6182558ce795b4d2b151030657eb03c7b7b80d
Matteo Toma <matteotoma98@hotmail.it>
2020-06-01 16:00:18 +0200

    Form cambiato con nuovo validator

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Session;
 use App\Models\Catalog;
 use App\Models\Resources\Product;
 use App\Models\Resources\Category;
@@ -19,7 +19,7 @@ class StaffController extends Controller {
         $this->middleware('can:isStaff');
         $this->_catalogModel = new Catalog; //metodo che viene attivato ogni volta che un'azione dell'admin viene richiesta
     }
-    
+
     public function index() {
         return view('staffdashboard');
     }
@@ -35,63 +35,83 @@ class StaffController extends Controller {
         return view('addproduct')
                         ->with('subCategories', $prodCats);
     }
-	public function editProduct($productCode) {
+
+    public function editProduct($productCode) {
         $prodCats = $this->_catalogModel->getSubCats()->pluck('nome', 'id');
-		$product = $this->_catalogModel->getProductByCode([$productCode]);
+        $product = $this->_catalogModel->getProductByCode([$productCode]);
         return view('addproduct')
                         ->with('subCategories', $prodCats)
-						->with('product', $product);
+                        ->with('product', $product);
     }
-	
-	public function completeMsg($id) {
-		$msg = 'message';
-		switch($id) {
-			case 0:
-				$msg = 'Prodotto aggiunto correttamente';
-				break;
-			case 1:
-				$msg = 'Prodotto aggiornato correttamente';
-				break;
-			case 2:
-				$msg = 'Prodotto eliminato correttamente';
-				break;
-                        case 3:
-				$msg = 'Categoria aggiunta correttamente';
-				break;
-			case 4:
-				$msg = 'Categoria aggiornata correttamente';
-				break;
-			case 5:
-				$msg = 'Categoria eliminata correttamente';
-				break;
+
+    public function completeMsg($id) {
+        $msg = 'message';
+        switch ($id) {
+            case 0:
+                $msg = 'Prodotto aggiunto correttamente';
+                break;
+            case 1:
+                $msg = 'Prodotto aggiornato correttamente';
+                break;
+            case 2:
+                $msg = 'Prodotto eliminato correttamente';
+                break;
+            case 3:
+                $msg = 'Categoria aggiunta correttamente';
+                break;
+            case 4:
+                $msg = 'Categoria aggiornata correttamente';
+                break;
+            case 5:
+                $msg = 'Categoria eliminata correttamente';
+                break;
             case 6:
-				$msg = 'Sottocategoria aggiunta correttamente';
-				break;
-			case 7:
-				$msg = 'Sottocategoria aggiornata correttamente';
-				break;
-			case 8:
-				$msg = 'Sottocategoria eliminata correttamente';
-				break;
-			default:
-				$msg = 'Error';
-				break;
-		}
-		return view('completemsg')
-						->with('message', $msg);
-	}
+                $msg = 'Sottocategoria aggiunta correttamente';
+                break;
+            case 7:
+                $msg = 'Sottocategoria aggiornata correttamente';
+                break;
+            case 8:
+                $msg = 'Sottocategoria eliminata correttamente';
+                break;
+            default:
+                $msg = 'Error';
+                break;
+        }
+        return view('completemsg')
+                        ->with('message', $msg);
+    }
 
+    public function storeProduct(NewProductRequest $request) {
 
-    public function storeProduct(NewProductRequest $request) { 
-        
-		$product = new Product;
+        $product = new Product;
         $product->fill($request->validated());
-		
+
         if ($request->hasFile('immagine')) {
             $image = $request->file('immagine');
             $imageName = $image->getClientOriginalName(); //estrae solo il nome dell'immagine
-			$product->immagine = $imageName;
-			$destinationPath = public_path() . '/images/products_images';
+            $product->immagine = $imageName;
+            $destinationPath = public_path() . '/images/products_images';
+            $image->move($destinationPath, $imageName);
+        }
+        $product->save();
+        // return response()-> view('home');
+
+      return response()->json(['redirect' => route('completemsg',0)]);
+        //senza override :     return redirect()->route('completemsg', 0);
+    }
+
+    public function saveProduct(NewProductRequest $request, $productCode) {
+
+
+        $product = $this->_catalogModel->getProductByCode([$productCode]);
+        $product->fill($request->validated());
+
+        if ($request->hasFile('immagine')) {
+            $image = $request->file('immagine');
+            $imageName = $image->getClientOriginalName(); //estrae solo il nome dell'immagine
+            $product->immagine = $imageName;
+            $destinationPath = public_path() . '/images/products_images';
             $image->move($destinationPath, $imageName);
         }
 
@@ -99,129 +119,108 @@ class StaffController extends Controller {
 
         return redirect()->route('completemsg', 0);
     }
-	
-	public function saveProduct(NewProductRequest $request, $productCode) { 
-        
-		
-		$product = $this->_catalogModel->getProductByCode([$productCode]);
-		$product->fill($request->validated());
-		
-        if ($request->hasFile('immagine')) {
-            $image = $request->file('immagine');
-            $imageName = $image->getClientOriginalName(); //estrae solo il nome dell'immagine
-			$product->immagine = $imageName;
-			$destinationPath = public_path() . '/images/products_images';
-            $image->move($destinationPath, $imageName);
-        } 
 
-        $product->save();
+    public function deleteProduct(Request $request) {
 
-        return redirect()->route('completemsg', 1);
+        $product = $this->_catalogModel->getProductByCode([$request->productCode]);
+        $destinationPath = public_path() . '/images/products_images/' . $product->immagine;
+        unlink($destinationPath);
+        $product->delete();
     }
-	
-	public function deleteProduct(Request $request) {
 
-		$product = $this->_catalogModel->getProductByCode([$request->productCode]);
-		$destinationPath = public_path() . '/images/products_images/' . $product->immagine;
-		unlink($destinationPath);
-		$product->delete();
-		
-    }
-    
     //Categorie
-    
+
     public function showCategoriesList() {
-        
+
         $cats = $this->_catalogModel->getCats();
         return view('selectcategory')->with('categories', $cats);
     }
-    
+
     public function addCategory() {
         return view('addcategory');
     }
-    
+
     public function editCategory($catId) {
 
-	$category = $this->_catalogModel->getCategoryByID([$catId]);
+        $category = $this->_catalogModel->getCategoryByID([$catId]);
         return view('addcategory')
-			->with('category', $category);
+                        ->with('category', $category);
     }
-    
-    public function storeCategory(NewCategoryRequest $request) { 
-        
-	$category = new Category;
+
+    public function storeCategory(NewCategoryRequest $request) {
+
+        $category = new Category;
         $category->fill($request->validated());
         $category->save();
 
         return redirect()->route('completemsg', 3);
     }
-    
-    public function saveCategory(NewCategoryRequest $request, $catId) { 
-       	
-	$category = $this->_catalogModel->getCategoryByID([$catId]);
-	$category->fill($request->validated());
- 
+
+    public function saveCategory(NewCategoryRequest $request, $catId) {
+
+        $category = $this->_catalogModel->getCategoryByID([$catId]);
+        $category->fill($request->validated());
+
         $category->save();
 
         return redirect()->route('completemsg', 4);
     }
-    
+
     public function deleteCategory(Request $request) {
 
-	$category = $this->_catalogModel->getCategoryByID([$request->categoryID]);
-	$category->delete();
-		
+        $category = $this->_catalogModel->getCategoryByID([$request->categoryID]);
+        $category->delete();
     }
-    
+
     //Sottocategorie
-    
+
     public function showSubcategoriesList() {
-        
+
         $subcats = $this->_catalogModel->getSubCats();
-		$cats = $this->_catalogModel->getCats();
+        $cats = $this->_catalogModel->getCats();
         return view('selectsubcategory')
-						->with('categories', $cats)
-						->with('subcategories', $subcats);
+                        ->with('categories', $cats)
+                        ->with('subcategories', $subcats);
     }
-    
+
     public function addSubcategory() {
-        $cats = $this->_catalogModel->getCats()->pluck('nome','id');
+        $cats = $this->_catalogModel->getCats()->pluck('nome', 'id');
         return view('addsubcategory')
                         ->with('categories', $cats);
     }
-    
+
     public function editSubcategory($subcategoryID) {
-        $cats = $this->_catalogModel->getCats()->pluck('nome','id');
-		$subcategory = $this->_catalogModel->getSubcategoryByID([$subcategoryID]);
+        $cats = $this->_catalogModel->getCats()->pluck('nome', 'id');
+        $subcategory = $this->_catalogModel->getSubcategoryByID([$subcategoryID]);
         return view('addsubcategory')
                         ->with('categories', $cats)
-						->with('subcategory', $subcategory);
+                        ->with('subcategory', $subcategory);
     }
-    
-    public function storeSubcategory(NewSubcategoryRequest $request) { 
-        
-	$subcategory = new SubCategory;
+
+    public function storeSubcategory(NewSubcategoryRequest $request) {
+
+        $subcategory = new SubCategory;
         $subcategory->fill($request->validated());
 
         $subcategory->save();
 
         return redirect()->route('completemsg', 6);
     }
-	
-	public function saveSubcategory(NewSubcategoryRequest $request, $subcategoryID) { 
-        
-	$subcategory = $this->_catalogModel->getSubcategoryByID([$subcategoryID]);
-	$subcategory->fill($request->validated());
+
+    public function saveSubcategory(NewSubcategoryRequest $request, $subcategoryID) {
+
+        $subcategory = $this->_catalogModel->getSubcategoryByID([$subcategoryID]);
+        $subcategory->fill($request->validated());
 
         $subcategory->save();
 
         return redirect()->route('completemsg', 7);
     }
-	
-	public function deleteSubcategory(Request $request) {
 
-		$subcategory = $this->_catalogModel->getSubcategoryByID([$request->subcategoryID]);
-		$subcategory->delete();
-		
+    public function deleteSubcategory(Request $request) {
+
+        $subcategory = $this->_catalogModel->getSubcategoryByID([$request->subcategoryID]);
+        $subcategory->delete();
     }
+
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,123 +1,223 @@
-$(document).ready(function() {
-	// When the user scrolls the page, execute myFunction
-	window.onscroll = function() {myFunction()};
+/* global formId */
+
+$(document).ready(function () {
+// When the user scrolls the page, execute myFunction
+    window.onscroll = function () {
+        myFunction();
+    };
     // When the window is resized, execute changeHeight
-	window.onresize = function() {changeHeight()};
-
-	// Get the navbar
-	var navbar = $("#topnav");
-	var main_content = $("#main");
-
-	// Get the offset position of the navbar
-	var sticky = navbar.offset().top;
+    window.onresize = function () {
+        changeHeight();
+    };
+    // Get the navbar
+    var navbar = $("#topnav");
+    var main_content = $("#main");
+    // Get the offset position of the navbar
+    var sticky = navbar.offset().top;
     // Get the height of the navbar
     var height = navbar.outerHeight();
-
-	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-	function myFunction() {
-		if (window.pageYOffset >= sticky) {
-			if (main_content!==null) main_content.css("padding", height+"px 0 0 0");
-			navbar.addClass("sticky");
-		} else {
-			navbar.removeClass("sticky");
-		}
-	}
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            if (main_content !== null)
+                main_content.css("padding", height + "px 0 0 0");
+            navbar.addClass("sticky");
+        } else {
+            navbar.removeClass("sticky");
+        }
+    }
     // Update the height of the navbar and the padding of the main box
-    function changeHeight(){
+    function changeHeight() {
         height = navbar.outerHeight();
         myFunction();
     }
-		
-	
-	//MENU CATEGORIE PRODOTTI
-	$("div.cat > img").on("click", function() {
-		$(this).toggleClass("rotate").next().slideToggle(200);
-	});
-	
-	
-	//PREVIEW FOTO NEL FORM PRODOTTI
-	function readURL(input) {
-		if (input.files && input.files[0]) {
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			$('#preview').attr('src', e.target.result);
-		}
-		reader.readAsDataURL(input.files[0]); // convert to base64 string
-		}
-	}
 
-	$("#image").change(function() {
-		readURL(this);
-	});
-	
-	//SELEZIONE PRODOTTI STAFF (usato anche per categorie e sottocategorie)
-	var productRows = $(".productrow");
-	var selectedProd = null;
-	productRows.on("click", function() {
-		productRows.not(this).removeClass("selected_row");
-		$(this).toggleClass("selected_row");
-		if($(this).hasClass("selected_row")) selectedProd = $(this).attr("id");
-		else selectedProd = null;
-	});
-	
-	$("#edit, #delete").on("click", function() {
-		if(selectedProd == null ) alert ("Non hai effettuato la selezione!");
-		else {
-			if($(this).attr("id") == "edit") document.location.href = "editproduct/" + selectedProd ;
-			else { //attr(id) == delete
-				if(confirm("Sei sicuro di voler eliminare questo prodotto?")) {
-					var token = $(this).data("token");					
-					$.ajax({
-						type: "DELETE",
-						url: './deleteproduct',
-						data: {'productCode': selectedProd, '_token' : token, '_method' : 'DELETE'},
-						success: function () { //Il return dal controller non funziona con Ajax
-							document.location.href = "completemsg/2";
-						}
-					});
-				}
-			}
-		}
-	});
-        
-        $("#editcat, #deletecat").on("click", function() {
-		if(selectedProd == null ) alert ("Non hai effettuato la selezione!");
-		else {
-			if($(this).attr("id") == "editcat") document.location.href = "editcategory/" + selectedProd ;
-			else { //attr(id) == delete
-				if(confirm("Sei sicuro di voler eliminare questa categoria?")) {
-					var token = $(this).data("token");					
-					$.ajax({
-						type: "DELETE",
-						url: './deletecategory',
-						data: {'categoryID': selectedProd, '_token' : token, '_method' : 'DELETE'},
-						success: function () { //Il return dal controller non funziona con Ajax
-							document.location.href = "completemsg/5";
-						}
-					});
-				}
-			}
-		}
-	});
-        
-        $("#editsub, #deletesub").on("click", function() {
-		if(selectedProd == null ) alert ("Non hai effettuato la selezione!");
-		else {
-			if($(this).attr("id") == "editsub") document.location.href = "editsubcategory/" + selectedProd ;
-			else { //attr(id) == delete
-				if(confirm("Sei sicuro di voler eliminare quests sottocategoria?")) {
-					var token = $(this).data("token");					
-					$.ajax({
-						type: "DELETE",
-						url: './deletesubcategory',
-						data: {'subcategoryID': selectedProd, '_token' : token, '_method' : 'DELETE'},
-						success: function () { //Il return dal controller non funziona con Ajax
-							document.location.href = "completemsg/8";
-						}
-					});
-				}
-			}
-		}
-	});
-	
+
+    //MENU CATEGORIE PRODOTTI
+    $("div.cat > img").on("click", function () {
+        $(this).toggleClass("rotate").next().slideToggle(200);
+    });
+    //PREVIEW FOTO NEL FORM PRODOTTI
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#preview').attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]); // convert to base64 string
+        }
+    }
+
+    $("#image").change(function () {
+        readURL(this);
+    });
+
+    //SELEZIONE PRODOTTI STAFF (usato anche per categorie e sottocategorie)
+    var productRows = $(".productrow");
+    var selectedProd = null;
+    productRows.on("click", function () {
+        productRows.not(this).removeClass("selected_row");
+        $(this).toggleClass("selected_row");
+        if ($(this).hasClass("selected_row"))
+            selectedProd = $(this).attr("id");
+        else
+            selectedProd = null;
+    });
+    $("#edit, #delete").on("click", function () {
+        if (selectedProd == null)
+            alert("Non hai effettuato la selezione!");
+        else {
+            if ($(this).attr("id") == "edit")
+                document.location.href = "editproduct/" + selectedProd;
+            else { //attr(id) == delete
+                if (confirm("Sei sicuro di voler eliminare questo prodotto?")) {
+                    var token = $(this).data("token");
+                    $.ajax({
+                        type: "DELETE",
+                        url: './deleteproduct',
+                        data: {'productCode': selectedProd, '_token': token, '_method': 'DELETE'},
+                        success: function () { //Il return dal controller non funziona con Ajax
+                            document.location.href = "completemsg/2";
+                        }
+                    });
+                }
+            }
+        }
+    });
+    $("#editcat, #deletecat").on("click", function () {
+        if (selectedProd == null)
+            alert("Non hai effettuato la selezione!");
+        else {
+            if ($(this).attr("id") == "editcat")
+                document.location.href = "editcategory/" + selectedProd;
+            else { //attr(id) == delete
+                if (confirm("Sei sicuro di voler eliminare questa categoria?")) {
+                    var token = $(this).data("token");
+                    $.ajax({
+                        type: "DELETE",
+                        url: './deletecategory',
+                        data: {'categoryID': selectedProd, '_token': token, '_method': 'DELETE'},
+                        success: function () { //Il return dal controller non funziona con Ajax
+                            document.location.href = "completemsg/5";
+                        }
+                    });
+                }
+            }
+        }
+    });
+    $("#editsub, #deletesub").on("click", function () {
+        if (selectedProd === null)
+            alert("Non hai effettuato la selezione!");
+        else {
+            if ($(this).attr("id") === "editsub")
+                document.location.href = "editsubcategory/" + selectedProd;
+            else { //attr(id) == delete
+                if (confirm("Sei sicuro di voler eliminare quests sottocategoria?")) {
+                    var token = $(this).data("token");
+                    $.ajax({
+                        type: "DELETE",
+                        url: './deletesubcategory',
+                        data: {'subcategoryID': selectedProd, '_token': token, '_method': 'DELETE'},
+                        success: function () { //Il return dal controller non funziona con Ajax
+                            document.location.href = "completemsg/8";
+                        }
+                    });
+                }
+            }
+        }
+    });
+
+    //VALIDAZIONE FORM IN JSON
+    $(":input").on('blur', function (event) {
+        var formElementId = $(this).attr('id');
+        doElemValidation(formElementId, actionUrl, formId);
+    });
+    $(".publish").on('click', function (event) //NON FUNZIONA!
+    {
+        event.preventDefault();
+        doFormValidation(actionUrl, formId);
+    });
+
+    function getErrorHtml(elemErrors) {
+        if ((typeof (elemErrors) === 'undefined') || (elemErrors.length < 1))
+            return;
+        var out = '';
+        for (var i = 0; i < elemErrors.length; i++) {
+            out += '<div class="errormsg">' + elemErrors[i] + '</div>';
+        }
+        return out;
+    }
+
+    function doElemValidation(id, actionUrl, formId) {
+
+        var formElems;
+
+        function addFormToken() {
+            var tokenVal = $("#" + formId + " input[name=_token]").val();
+            formElems.append('_token', tokenVal);
+        }
+
+        function sendAjaxReq() {
+            $.ajax({
+                type: 'POST',
+                url: actionUrl,
+                data: formElems,
+                dataType: "json",
+                error: function (data) {
+                    if (data.status === 422) {
+                        var errMsgs = JSON.parse(data.responseText);
+                        $("#" + id).next('.errormsg').html(' ');
+                        $("#" + id).after(getErrorHtml(errMsgs[id]));
+                    }
+                },
+                contentType: false,
+                processData: false
+            });
+        }
+
+        var elem = $("#" + formId + " :input[name=" + id + "]");
+        if (elem.attr('type') === 'file') {
+            // elemento di input type=file valorizzato
+            if (elem.val() !== '') {
+                inputVal = elem.get(0).files[0];
+            } else {
+                inputVal = new File([""], ""); //rappresenta la mappatura di un file all'interno della form
+            } //se l'utente non mette il nome del file si genera un file senza contenuto e nome
+        } else {
+            // elemento di input type != file
+            inputVal = elem.val();
+        }
+        formElems = new FormData();
+        formElems.append(id, inputVal);
+        addFormToken();
+        sendAjaxReq();
+
+    }
+
+
+    function doFormValidation(actionUrl, formId) {
+
+        var form = new FormData(document.getElementById(formId));
+        $.ajax({
+            type: 'POST',
+            url: actionUrl,
+            data: form,
+            dataType: "json",
+            error: function (data) {
+                if (data.status === 422) {
+                    var errMsgs = JSON.parse(data.responseText);
+                    $.each(errMsgs, function (id) {
+                        $("#" + id).next('.errormsg').html(' ');
+                        $("#" + id).after(getErrorHtml(errMsgs[id]));
+                    });
+                }
+            },
+            success: function (data) {
+                window.location.replace(data.redirect);
+            },
+            contentType: false,
+            processData: false
+        });
+    }
+
 });
