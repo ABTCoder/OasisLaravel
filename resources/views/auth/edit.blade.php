0d583390292f22cc05ff3779a9aaabc6d86a5f93
Matteo Toma <matteotoma98@hotmail.it>
2020-06-13 11:08:30 +0200

    CAMBIATO LINK DELLA RELAZIONE

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -18,35 +18,36 @@
     {{ Form::model($user, array('route' => 'editaccount.save', 'id' => 'editform')) }}
     @csrf
     {{ Form::text('email', null, ['placeholder' => 'Email', 'id' => 'email']) }}
-    <div class='errormsg' id="error_msg"></div>
+    <div id='error_email' class="errormsg"></div>
     <br>
     {{ Form::password('oldpassword', ['placeholder' => 'Vecchia password', 'id' => 'oldpassword']) }}
-    <div class='errormsg' id="error_msg"></div>
+    <div id='error_oldpassword' class="errormsg"></div>
     <br>
     {{ Form::password('password', ['placeholder' => 'Nuova password', 'id' => 'password']) }}
-    <div class='errormsg' id="error_msg"></div>
+    <div id='error_password' class="errormsg"></div>
     <br>
     {{ Form::password('password_confirmation', ['placeholder' => 'Conferma Password', 'id' => 'password_confirmation']) }}
     <br>
     {{ Form::text('nome', null, ['placeholder' => 'Nome', 'id' => 'nome']) }}
-    <div class='errormsg' id="error_msg"></div>
+    <div id='error_nome' class="errormsg"></div>
     <br>
     {{ Form::text('cognome', null, ['placeholder' => 'Cognome', 'id' => 'cognome']) }}
-    <div class='errormsg' id="error_msg"></div>
+    <div id='error_cognome' class="errormsg"></div>
     <br>
     {{ Form::text('residenza', null, ['placeholder' => 'Indirizzo di residenza', 'id' => 'residenza']) }}
-    <div class='errormsg' id="error_msg"></div>
+    <div id='error_residenza' class="errormsg"></div>
     <br>
-    <div class='errormsg' id="error_msg"></div>
+    <div id='error_msg' class="errormsg"></div>
     <div class="date_job">
         {{ Form::date('data_nasc', null, ['placeholder' => 'Data di nascita', 'id' => 'data_nasc']) }}
         {{ Form::select('occupazione', ['Studente' => 'Studente', 'Operaio' => 'Operaio', 'Libero professionista' => 'Libero professionista', 
 										'Pubblica sicurezza' => 'Pubblica Sicurezza', 'Medico' => 'Medico', 'Impiegato' => 'Impiegato',
 										'Insegnante' => 'Insegnante'], null, ['placeholder' => 'Professione','id' => 'occupazione']) }}
+        <div id='error_data_nasc' class="errormsg"></div>
+        <br>
+        <div id='error_occupazione' class="errormsg"></div>
     </div>
-    <div class='errormsg' id="error_msg"></div>
-    <br>
-    <div class='errormsg' id="error_msg"></div>
+
     <br>
     {{ Form::submit('Modifica', ['id' => 'adduser']) }}
     <br>
